c09ff39a3c71e48ccdeaca436bbd748b80e4c19c
saicharan <62795476+saicharan-gouru@users.noreply.github.com>
2021-05-15 19:03:27 +0530

    Update ReverseTheArray.py

--- python/ReverseTheArray.py
+++ python/ReverseTheArray.py
@@ -1,7 +1,7 @@
 n=int(input("Enter Number Of Elements:"))
 OriginalArray = []
 for i in range(n):
-  OriginalArray.append(int(input()))
+  OriginalArray.append(int(input("Enter "+ str(i+1) +" Element Into Array:")))
 ReversedArray=OriginalArray[::-1]
 print("Origanal Array is ",OriginalArray)
 print("Reversed Array is ",ReversedArray)
