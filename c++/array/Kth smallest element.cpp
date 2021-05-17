1ef166e5698e3510d9c511c0dbf3236ba45072c7
Raunak <raunakpandey1007@gmail.com>
2021-05-17 12:19:23 +0530

    Kth smallest element program using C++

---
+++ c++/array/Kth smallest element.cpp
@@ -0,0 +1,26 @@
+/* Kth smallest element
+Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+*/
+//Link : https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
+
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+int kthSmallest(int arr[], int l, int r, int k) {
+    //code here
+       sort(arr,arr+r+1);
+       
+       return arr[k-1];
+}
