b9bd600ebdfb6403bee08f8dfaeb867aa1fdba26
Raunak Pandey <65064180+RAUNAK-PANDEY@users.noreply.github.com>
2021-05-29 01:38:19 +0530

    Update Longest Palindrome in a String.cpp

--- c++/string/Longest Palindrome in a String.cpp
+++ c++/string/Longest Palindrome in a String.cpp
@@ -1,4 +1,4 @@
-//Link : 
+//Link : https://practice.geeksforgeeks.org/problems/longest-palindrome-in-a-string3411/1
 /*
 Given a string S, find the longest palindromic substring in S. Substring of string S: S[ i . . . . j ] where 0 ≤ i ≤ j < len(S). Palindrome string: A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S. Incase of conflict, return the substring which occurs first ( with the least starting index).
 
