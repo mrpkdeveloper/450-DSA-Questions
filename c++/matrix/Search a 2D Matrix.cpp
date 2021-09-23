facac04962746951d576aabbf181ae45dc2f33f6
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-23 18:05:33 +0530

    Update Search a 2D Matrix.cpp
    A approach using using binary search on the whole matrix.

--- c++/matrix/Search a 2D Matrix.cpp
+++ c++/matrix/Search a 2D Matrix.cpp
@@ -30,3 +30,23 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
                return true;
             return false;
     }
+
+//approach using binary search on whole matrix.
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int low=0;
+        int high=m*n-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(matrix[mid/m][mid%m]==target)
+                return true;
+            else if(matrix[mid/m][mid%m]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return false;
+        
+    }
